d567c00222292e96d86811f7165ba9e0bf0bc538
Mirjalol002 <101446131+Mirjalol002@users.noreply.github.com>
2023-03-28 12:35:06 +0500

    I added properties "Lesson, Group Attendance"

--- EduCRM/EduCRM.Domain/Entities/Attendance.cs
+++ EduCRM/EduCRM.Domain/Entities/Attendance.cs
@@ -3,5 +3,9 @@
     public class Attendance
     {
         public int Id { get; set; }
+        public int StudentId { get; set; }
+        public int LessonId { get; set; }
+        public bool HasParticipated { get; set; }
+
     }
 }

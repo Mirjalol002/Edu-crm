96effbea4b84921db7d181639fdb38cbb75ababd
Mirjalol002 <101446131+Mirjalol002@users.noreply.github.com>
2023-03-28 17:21:27 +0500

    I add authservice haha

--- EduCRM/EduCRM.Application/Abstractions/IHashProvider.cs
+++ EduCRM/EduCRM.Application/Abstractions/IHashProvider.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EduCRM.Application.Abstractions
+﻿namespace EduCRM.Application.Abstractions
 {
     public interface IHashProvider
     {
--- EduCRM/EduCRM.Infrastructure/Services/AuthService.cs
+++ EduCRM/EduCRM.Infrastructure/Services/AuthService.cs
@@ -1,7 +1,34 @@
-﻿namespace EduCRM.Infrastructure.Services
-{
-    public class AuthService
-    {
+﻿using EduCRM.Application.Abstractions;
+using EduCRM.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
 
+namespace EduCRM.Infrastructure.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly IHashProvider _hashProvider;
+        private readonly ITokenService _tokenService;
+
+        public AuthService(ApplicationDbContext dbContext, IHashProvider hashProvider, ITokenService tokenService)
+        {
+            _dbContext = dbContext;
+            _hashProvider = hashProvider;
+            _tokenService = tokenService;
+        }
+        public async Task<string> LoginAsync(string username, string password)
+        {
+            var user = await _dbContext.Users.FirstOrDefaultAsync(x => x.UserName == username);
+            if (user == null)
+            {
+                throw new Exception("User not found");
+            }
+            if (user.PasswordHash != _hashProvider.GetHash(password))
+            {
+                throw new Exception("Password is wrong");
+            }
+            return _tokenService.GenerateAccessToken(user);
+        }
     }
 }

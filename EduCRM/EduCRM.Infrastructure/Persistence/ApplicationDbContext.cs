341913b1a931f6fb99bc8667c5700d0317727eb4
Mirjalol002 <101446131+Mirjalol002@users.noreply.github.com>
2023-03-28 17:08:09 +0500

    some changes to my rank 1

---
+++ EduCRM/EduCRM.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using EduCRM.Application.Abstractions;
+using EduCRM.Domain.Entities;
+using EduCRM.Infrastructure.Persistence.EntityTypeConfigurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduCRM.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+        
+        public DbSet<User> Users { get; set; }
+        public DbSet<Attendance> Attendances { get ; set ; }
+        public DbSet<Lesson> Lessons { get ; set ; }
+        public DbSet<Group> Groups { get ; set ; }
+        public DbSet<Student> Students { get ; set ; }
+        public DbSet<StudentGroup> StudentGroups { get ; set ; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new UserEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new AttendanceEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new  LessonEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new GroupEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new  StudentEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new StudentGroupEntityTypeConfiguration());
+        }
+    }
+}

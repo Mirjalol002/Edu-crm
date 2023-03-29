2ccc00193d1054ad4ce05d1655284d5533171fbd
Mirjalol002 <101446131+Mirjalol002@users.noreply.github.com>
2023-03-29 16:15:15 +0500

    some changes to my project 04:15

---
+++ EduCRM/EduCRM.Application/Models/AddStudentGroupModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EduCRM.Application.Models
+{
+    public class AddStudentGroupModel
+    {
+        public int StudentId { get; set; }
+        public bool IsPaid { get; set; }
+        public DateTime JoinedDate { get; set; }
+    }
+}

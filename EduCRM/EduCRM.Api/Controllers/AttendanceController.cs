34ed68b6a283578aed550ad2606d5897fa8ff036
Mirjalol002 <101446131+Mirjalol002@users.noreply.github.com>
2023-04-01 16:16:42 +0500

    I added Attendence Controller to my project

---
+++ EduCRM/EduCRM.Api/Controllers/AttendanceController.cs
@@ -0,0 +1,23 @@
+﻿using EduCRM.Application.Abstractions;
+using EduCRM.Application.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EduCRM.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AttendanceController : ControllerBase
+    {
+        private readonly IAttendanceService _attendanceService;
+        public AttendanceController(IAttendanceService attendanceService)
+        {
+            _attendanceService = attendanceService;
+        }
+        [HttpPost("Check")]
+        public async Task<IActionResult> Check(DoAttendanceCheckModel checkModel)
+        {
+            await _attendanceService.CheckAsync(checkModel);
+            return Ok();
+        }
+    }
+}

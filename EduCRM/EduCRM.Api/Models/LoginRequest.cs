850bf63ff37c048fe7792eaa30b70b121473d205
Mirjalol002 <101446131+Mirjalol002@users.noreply.github.com>
2023-04-05 15:04:16 +0500

    the last changed to my project

---
+++ EduCRM/EduCRM.Api/Models/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace EduCRM.Api.Models
+{
+    public class LoginRequest
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
